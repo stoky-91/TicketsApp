233c901fb99b9049def72e997423bb17645d7a9d
Martin Stoklasa <martin.stoklasa@artin.cz>
2024-07-16 20:32:56 +0200

    Unversioned Files

---
+++ TicketsApp/TicketsApp/src/test/java/com/tickets/TicketsApp/TicketsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tickets.TicketsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
